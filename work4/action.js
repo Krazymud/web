cace4d020d23cabf29c89553855a3614ec948b9a
Eadric <cy735322470@gmail.com>
2017-10-17 23:41:52 +0800

    change

--- work4/action.js
+++ work4/action.js
@@ -1,5 +1,19 @@
-copy = function(x){
-    document.getElementById("cal").value += document.getElementsByName("display")[x].value;
+var ele = document.getElementsByTagName("button");
+for (var i = 0; i < ele.length; i++) {
+    if (i >= 0 && i <= 13)
+        ele[i].addEventListener("click", copy);
+    else if (i >= 15 && i <= 17)
+        ele[i].addEventListener("click", copy);
+    else if (i == 14)
+        ele[i].addEventListener("click", back);
+    else if (i == 18)
+        ele[i].addEventListener("click", clr);
+    else
+        ele[i].addEventListener("click", calc);
+}
+
+function copy(){
+    document.getElementById("cal").value += this.value;
 }
 
 document.onkeydown = function(event){
@@ -7,8 +21,7 @@ document.onkeydown = function(event){
         calc();
 }
 
-back = function(){
-
+function back(){
     var string = document.getElementById("cal").value;
     var x = document.getElementById("cal").value.length;
     var str = string.substring(0, x - 1);
@@ -16,11 +29,11 @@ back = function(){
         document.getElementById("cal").value = str;
 }
 
-clr = function(){
+function clr(){
     document.getElementById("cal").value = "";
 }
 
-calc = function(){
+function calc(){
     var str = document.getElementById("cal").value;
     var sign = true;
     for(var n = 0; n < str.length; n++){
@@ -43,3 +56,5 @@ calc = function(){
         clr();
     }
 }
+
+
